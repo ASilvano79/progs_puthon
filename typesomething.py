de93707f32f74902e8ef89ae52b66f7b68cc69bf
Anderson Silvano <anderson.silvano@gmail.com>
2024-06-20 18:03:53 -0300

    Versão final Chute Certo

--- typesomething.py
+++ typesomething.py
@@ -1,62 +1,60 @@
-import random
+from random import randint
 from typesomething_logo import logo
 from replit import clear
 
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
 tentativas = 0
-def game():    
-    print(logo)
-    print("Bem vindo ao Jogo de Descobrir um Número.\nEu estou pensando em um número entre 1 e 100.")
-    random_number = random.randint(1,100)
-    #print(f"Random Number: {random_number}")
-    global tentativas
+
+def seta_dificuldade():
     nivel = input("Escolha um nível de dificuldade. Digite 'easy' ou 'hard': ")
     if nivel == "easy":
-        tentativas = 10
+        return EASY_LEVEL_TURNS
     elif nivel == "hard":
-        tentativas = 5
+        return HARD_LEVEL_TURNS
     else:
-        tentativas = 0    
-        
-    def show_tentativas():
-        global tentativas
-        print(f"Você tem {tentativas} tentativas para acertar o número")
-        
-    def diminui_tentativas():
-        global tentativas
-        tentativas -= 1
-        
-    def chute():
-        chute = int(input("Chute um número: "))
-        return chute
+        return 0    
+    
+def show_tentativas(tentativas):
+    print(f"Você tem {tentativas} tentativas para acertar o número")
+    
+def chute():
+    chute = int(input("Chute um número: "))
+    return chute
 
-    def analisa_result(chute):
-        global tentativas
-        if chute == random_number:
-            print(f"Você é a propria Mãe Dinah. Acertou o número: {chute}")
-            tentativas = 0
-        elif chute > random_number:
-            print("Você chutou um número MAIOR!")
-            diminui_tentativas()
-            if tentativas > 0:
-                print("Chute de novo!")
-            
-        elif chute < random_number:
-            print("Você chutou um número MENOR!")
-            diminui_tentativas()
-            if tentativas > 0:
-                print("Chute de novo!")
-        
-    while tentativas > 0:    
-        show_tentativas()
+def analisa_result(random_number,chute,chutes):
+    if chute > random_number:
+        print("Você chutou um número MAIOR!")
+        return chutes - 1
+    elif chute < random_number:
+        print("Você chutou um número MENOR!")
+        return chutes - 1
+    else:
+        print(f"Você é a propria Mãe Dinah. Acertou o número: {chute}")
+        return 0
+
+def game():    
+    
+    print(logo)
+    print("Bem vindo ao Jogo de Descobrir um Número.\nEu estou pensando em um número entre 1 e 100.")
+    random_number = randint(1,100)
+    #print(f"Random Number: {random_number}")
+    tentativas = seta_dificuldade()
+    opcao = 0
+    
+    while random_number != opcao:    
+        show_tentativas(tentativas)
         opcao = chute()
-        analisa_result(opcao)
-
+        tentativas = analisa_result(random_number,opcao,tentativas)
+        if tentativas == 0:
+            jogar = input("O jogo acabou. Deseja jogar de novo? Digite 'sim' ou 'não'.\n")
+            if jogar == "sim":
+                clear()
+                game()
+            else:
+                print("Tchau!!!")
+                return
 game()
 
-if tentativas == 0:
-    jogar = input("O jogo acabou. Deseja jogar de novo? Digite 'sim' ou 'não'.\n")
-    if jogar == "sim":
-        clear()
-        game()
-    else:
-        print("Tchau!!!")
+
